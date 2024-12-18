20e9e99ad2ae6015b8e96594add297ca6c68e07e
Alejandro Piña <aleprodev@gmail.com>
2024-12-18 13:50:05 -0300

    left and right images

--- src/components/client/SimulateYourCredit.tsx
+++ src/components/client/SimulateYourCredit.tsx
@@ -1,57 +1,69 @@
 import React from 'react';
 import CustomButton from '../shared/CustomButton';
+import left from '../../assets/images/left.png';
+import right from '../../assets/images/right.png';
 
 export default function SimulateYourCredit() {
   return (
-    <div className="flex justify-center items-center px-5">
-      <div className="flex flex-col space-y-7 justify-center items-center max-w-[480px]">
-        <h2 className="text-customPurple-600 font-bold text-center lg:text-right text-2xl uppercase">
-          Simula tu crédito
-        </h2>
-        <p className="text-customPurple-600 text-normal">Monto a solicitar:</p>
-        <div className="bg-customPurple-600 w-full py-3 rounded-xl text-white text-2xl text-center font-black">
-          <p>$ 5,000.00</p>
-        </div>
-        <div className="flex flex-col justify-between rounded-xl p-px bg-white drop-shadow-lg">
-          <div className="flex justify-center items-center p-3 py-7">
-            <p className="text-smaller text-customBlack-500 font-medium leading-7">
-              ¿En cuantas semanas quieres pagar?
-            </p>
+    <div className="relative">
+      <div className="flex justify-center items-center px-5">
+        <div className="flex flex-col space-y-7 justify-center items-center max-w-[480px]">
+          <h2 className="text-customPurple-600 font-bold text-center lg:text-right text-2xl uppercase">
+            Simula tu crédito
+          </h2>
+          <p className="text-customPurple-600 text-normal">
+            Monto a solicitar:
+          </p>
+          <div className="bg-customPurple-600 w-full py-3 rounded-xl text-white text-2xl text-center font-black">
+            <p>$ 5,000.00</p>
           </div>
-          <div className="flex justify-between px-8 items-center p-[14px] rounded-xl bg-customBlue-600">
-            <p className="text-xl text-white font-bold">16 semanas</p>
-          </div>
-        </div>
-        <div className="flex justify-center space-x-4">
           <div className="flex flex-col justify-between rounded-xl p-px bg-white drop-shadow-lg">
-            <div className="flex justify-center items-center p-3">
+            <div className="flex justify-center items-center p-3 py-7">
               <p className="text-smaller text-customBlack-500 font-medium leading-7">
-                Pago por semana en el plazo seleccionado:
+                ¿En cuantas semanas quieres pagar?
               </p>
             </div>
             <div className="flex justify-between px-8 items-center p-[14px] rounded-xl bg-customBlue-600">
-              <p className="text-xl text-white font-bold">$0</p>
+              <p className="text-xl text-white font-bold">16 semanas</p>
             </div>
           </div>
-          <div className="flex flex-col justify-between rounded-xl p-px bg-white drop-shadow-lg">
-            <div className="flex justify-center items-center p-3">
-              <p className="text-smaller text-customBlack-500 font-medium leading-7">
-                Monto total a pagar en el plazo seleccionado:
-              </p>
+          <div className="flex justify-center space-x-4">
+            <div className="flex flex-col justify-between rounded-xl p-px bg-white drop-shadow-lg">
+              <div className="flex justify-center items-center p-3">
+                <p className="text-smaller text-customBlack-500 font-medium leading-7">
+                  Pago por semana en el plazo seleccionado:
+                </p>
+              </div>
+              <div className="flex justify-between px-8 items-center p-[14px] rounded-xl bg-customBlue-600">
+                <p className="text-xl text-white font-bold">$0</p>
+              </div>
             </div>
-            <div className="flex justify-between px-8 items-center p-[14px] rounded-xl bg-customBlue-600">
-              <p className="text-xl text-white font-bold">$0</p>
+            <div className="flex flex-col justify-between rounded-xl p-px bg-white drop-shadow-lg">
+              <div className="flex justify-center items-center p-3">
+                <p className="text-smaller text-customBlack-500 font-medium leading-7">
+                  Monto total a pagar en el plazo seleccionado:
+                </p>
+              </div>
+              <div className="flex justify-between px-8 items-center p-[14px] rounded-xl bg-customBlue-600">
+                <p className="text-xl text-white font-bold">$0</p>
+              </div>
             </div>
           </div>
+          <div className="w-fit pt-10">
+            <CustomButton
+              textColor="white"
+              text="Solicita tu crédito"
+              cb={() => {}}
+              bgColor="customBlue-500"
+            />
+          </div>
         </div>
-        <div className="w-fit pt-10">
-          <CustomButton
-            textColor="white"
-            text="Solicita tu crédito"
-            cb={() => {}}
-            bgColor="customBlue-500"
-          />
-        </div>
+      </div>
+      <div className="hidden xl:block absolute top-16 left-0">
+        <img src={left} alt="" />
+      </div>
+      <div className="hidden xl:block absolute top-1/4 right-0">
+        <img src={right} alt="" />
       </div>
     </div>
   );
--- src/components/gestor/WinUpTo.tsx
+++ src/components/gestor/WinUpTo.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import CustomButton from '../shared/CustomButton';
+import left from '../../assets/images/left.png';
+import right from '../../assets/images/right.png';
 
 export default function WinUpTo() {
   return (
-    <div className="flex justify-center items-center px-5 lg:pt-8">
-      <div className="flex flex-col space-y-7 justify-center items-center max-w-[480px]">
-        <h2 className="text-customPurple-600 font-bold text-center lg:text-right text-2xl uppercase">
-          Llega a ganar hasta
-        </h2>
-        <div className="bg-customPurple-600 w-fit py-3 px-5 sm:px-10 rounded-xl text-white text-2xl text-center font-black">
-          <p>$ 28,000.00</p>
+    <div className="relative">
+      <div className="flex justify-center items-center px-5 lg:pt-8">
+        <div className="flex flex-col space-y-7 justify-center items-center max-w-[480px]">
+          <h2 className="text-customPurple-600 font-bold text-center lg:text-right text-2xl uppercase">
+            Llega a ganar hasta
+          </h2>
+          <div className="bg-customPurple-600 w-fit py-3 px-5 sm:px-10 rounded-xl text-white text-2xl text-center font-black">
+            <p>$ 28,000.00</p>
+          </div>
+          <p className="text-customPurple-600 font-bold text-center lg:text-right text-2xl uppercase">
+            Mensuales <span className="text-customBlue-500">*</span>
+          </p>
+          <p className="text-customPurple-600 text-tight text-center">
+            *Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
         </div>
-        <p className="text-customPurple-600 font-bold text-center lg:text-right text-2xl uppercase">
-          Mensuales <span className="text-customBlue-500">*</span>
-        </p>
-        <p className="text-customPurple-600 text-tight text-center">
-          *Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+      </div>
+      <div className="hidden xl:block absolute top-0 left-0">
+        <img src={left} alt="" width="70%" />
+      </div>
+      <div className="hidden xl:block absolute top-1/4 right-0">
+        <img src={right} alt="" />
       </div>
     </div>
   );
